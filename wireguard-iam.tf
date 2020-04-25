c1c5a21747ac08c06ee469136b6f5b07925cfc09
wchatx <wchatx@gmail.com>
2020-04-25 06:59:59 -0500

    set count

--- wireguard-iam.tf
+++ wireguard-iam.tf
@@ -25,6 +25,7 @@ resource "aws_iam_policy" "wireguard_policy" {
   name        = "tf-wireguard-${var.env}"
   description = "Terraform Managed. Allows Wireguard instance to attach EIP."
   policy      = data.aws_iam_policy_document.wireguard_policy_doc.json
+  count       = 1
 }
 
 resource "aws_iam_role" "wireguard_role" {
@@ -32,14 +33,17 @@ resource "aws_iam_role" "wireguard_role" {
   description        = "Terraform Managed. Role to allow Wireguard instance to attach EIP."
   path               = "/"
   assume_role_policy = data.aws_iam_policy_document.ec2_assume_role.json
+  count              = 1
 }
 
 resource "aws_iam_role_policy_attachment" "wireguard_roleattach" {
   role       = aws_iam_role.wireguard_role[0].name
   policy_arn = aws_iam_policy.wireguard_policy[0].arn
+  count      = 1
 }
 
 resource "aws_iam_instance_profile" "wireguard_profile" {
   name  = "tf-wireguard-${var.env}"
   role  = aws_iam_role.wireguard_role[0].name
+  count = 1
 }
